ce22943ad8afa54e6fe0c33cb92b784f65dfb6bc
Trường giang <giangkoolpark@gmail.com>
2018-05-19 13:54:57 +0700

    update

--- coupon.js
+++ coupon.js
@@ -25,8 +25,8 @@ io.on('connection', function (socket) {
         socket.broadcast.emit('show_coupon_accept');
     })
 
-    socket.on('user_use_coupon', function () {
-        socket.broadcast.emit('show_coupon_for_shop');
+    socket.on('user_use_coupon', function (shop_id) {
+        socket.broadcast.emit('show_coupon_for_shop', shop_id);
     })
 })
 /*
