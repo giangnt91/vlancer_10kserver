54e99e5b83f2d4bac239c119f07394ea64040e9e
Trường giang <giangkoolpark@gmail.com>
2018-05-22 15:35:18 +0700

    as

--- controller/shopCtrl.js
+++ controller/shopCtrl.js
@@ -655,26 +655,25 @@ module.exports = {
         })
     },
     RemoveCouponShopreject: function (req, res) {
-        shop_model.find({ shopId: req.body.shopId }, function (err, data) {
+        shop_model.find({ shopId: req.body.shopid }, function (err, data) {
             if (err) {
                 response = { 'error_code': 1, 'message': 'error fetching data' };
             } else {
-                res.status(200).json(data);
-                // var shop_use_coupon = data[0].shop_use_coupon;
-                // shop_use_coupon.forEach(element => {
-                //     if (element._id === req.body.couponId) {
-                //         delete (element);
-                //     }
-                // });
-                // data[0].shop_use_coupon = shop_use_coupon;
-                // data[0].save(function (err) {
-                //     if (err) {
-                //         response = { 'error_code': 2, 'message': err }
-                //     } else {
-                //         response = { 'error_code': 0, 'message': 'coupon remove success' };
-                //     }
-                //     res.status(200).json(shop_use_coupon);
-                // })
+                var shop_use_coupon = data[0].shop_use_coupon;
+                shop_use_coupon.forEach(element => {
+                    if (element._id === req.body.couponId) {
+                        delete (element);
+                    }
+                });
+                data[0].shop_use_coupon = shop_use_coupon;
+                data[0].save(function (err) {
+                    if (err) {
+                        response = { 'error_code': 2, 'message': err }
+                    } else {
+                        response = { 'error_code': 0, 'message': 'coupon remove success' };
+                    }
+                    res.status(200).json(shop_use_coupon);
+                })
             }
         })
     }
