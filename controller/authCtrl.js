1bc0c4349fb26ea1fba0ce4083862652fcdbcafe
Trường giang <giangkoolpark@gmail.com>
2018-05-23 01:03:57 +0700

    s

--- controller/authCtrl.js
+++ controller/authCtrl.js
@@ -588,6 +588,7 @@ module.exports = {
                         if (shop_use_coupon.length > 0) {
                             shop_use_coupon.forEach(element => {
                                 if (element._id.toString() === req.body.couponId) {
+                                    console.log(element)
                                     _coupon = {
                                         shop_name: element.coupon.shop_name,
                                         shop_cover: element.coupon.shop_cover,
