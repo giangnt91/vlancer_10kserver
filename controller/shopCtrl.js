a7daea84d51d88eb4f79ad44bad5e94e0661d10a
Trường giang <giangkoolpark@gmail.com>
2018-05-22 15:30:15 +0700

    as

--- controller/shopCtrl.js
+++ controller/shopCtrl.js
@@ -659,21 +659,22 @@ module.exports = {
             if (err) {
                 response = { 'error_code': 1, 'message': 'error fetching data' };
             } else {
-                var shop_use_coupon = data[0].shop_use_coupon;
-                shop_use_coupon.forEach(element => {
-                    if (element._id === req.body.couponId) {
-                        delete (element);
-                    }
-                });
-                data[0].shop_use_coupon = shop_use_coupon;
-                data[0].save(function (err) {
-                    if (err) {
-                        response = { 'error_code': 2, 'message': err }
-                    } else {
-                        response = { 'error_code': 0, 'message': 'coupon remove success' };
-                    }
-                    res.status(200).json(shop_use_coupon);
-                })
+                res.status(200).json(data);
+                // var shop_use_coupon = data[0].shop_use_coupon;
+                // shop_use_coupon.forEach(element => {
+                //     if (element._id === req.body.couponId) {
+                //         delete (element);
+                //     }
+                // });
+                // data[0].shop_use_coupon = shop_use_coupon;
+                // data[0].save(function (err) {
+                //     if (err) {
+                //         response = { 'error_code': 2, 'message': err }
+                //     } else {
+                //         response = { 'error_code': 0, 'message': 'coupon remove success' };
+                //     }
+                //     res.status(200).json(shop_use_coupon);
+                // })
             }
         })
     }
