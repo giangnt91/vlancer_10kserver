a142d0895ffd0ec4dd5863e676ed36e0923dd830
Trường giang <giangkoolpark@gmail.com>
2018-05-30 22:02:48 +0700

    update code 2

--- controller/codeCtrl.js
+++ controller/codeCtrl.js
@@ -27,6 +27,8 @@ module.exports = {
     // create basic
     basic_code: function (req, res) {
         create_basic_code(req.body.shopId, req.body.shopImg, req.body.info_coupon, req.body.code_coupon, req.body.expire_day, req.body.shopUrl);
+        response = { 'error_code': 0, 'message': 'new Basic coupon  is created !' }
+        res.status(200).json(response);
     },
 
     // get basic
