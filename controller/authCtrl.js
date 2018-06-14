83746ecbc8f04286cc7fb2bef09d82db93d05fc2
Trường giang <giangkoolpark@gmail.com>
2018-06-14 20:40:26 +0700

    test

--- controller/authCtrl.js
+++ controller/authCtrl.js
@@ -642,14 +642,52 @@ module.exports = {
             if (err) {
                 response = { 'error_code': 1, 'message': 'error fetching data' };
             } else {
-                var use_coupon = data.use_coupon;
-                use_coupon.forEach(element => {
+                data.use_coupon.forEach(element => {
                     if (element._id === req.body.couponId) {
-                        element.rating = req.body.rating;
-                        element.feedback = req.body.feedback;
+                        the_new = {
+                            shop_name: element.shop_name,
+                            shop_cover: element.shop_cover,
+                            shop_avatar: element.shop_avatar,
+                            shop_id: element.shop_id,
+                            coupon_info: element.coupon_info,
+                            value: element.value,
+                            class_user: [
+                                {
+                                    id: element.class_user[0].id,
+                                    name: element.class_user[0].name
+                                }
+                            ],
+                            release_day: element.release_day,
+                            time_expire: element.time_expire,
+                            the_issuer: [
+                                {
+                                    id: element.the_issuer[0].id,
+                                    name: element.the_issuer[0].name
+                                }
+                            ],
+                            status_coupon: [
+                                {
+                                    id: 0,
+                                    status: "Đã sử dụng"
+                                }
+                            ],
+                            userid_get_coupon: element.userid_get_coupon,
+                            time_user_get: element.time_user_get,
+                            time_user_use: _today,
+                            rating: req.body.rating,
+                            rfeedback: [
+                                {
+                                    name: element.rfeedback[0].name,
+                                    id: element.rfeedback[0].id
+                                }
+                            ],
+                            feedback: req.body.feedback,
+                            approved: element.approved,
+                            _id: element._id
+                        }
+                        element = the_new;
                     }
                 });
-                data.use_coupon = use_coupon;
                 data.save(function (err) {
                     if (err) {
                         response = { 'error_code': 3, 'message': 'error update data' };
