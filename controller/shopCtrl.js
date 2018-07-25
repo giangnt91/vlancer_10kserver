7767e4e97787f476ab438fd8559cfce2aff0a6ee
Trường giang <giangkoolpark@gmail.com>
2018-07-25 14:00:36 +0700

    a

--- controller/shopCtrl.js
+++ controller/shopCtrl.js
@@ -775,90 +775,90 @@ module.exports = {
             if (err) {
                 response = { 'error_code': 1, 'message': 'error fetching data' };
             } else {
-                if (data !== "") {
-                    var user_get_coupon = data[0].user_get_coupon;
-                    var shop_use_coupon = data[0].shop_use_coupon;
-                    var _today = dateFormat(new Date(), "dd/mm/yyyy");
-                    //chuyển coupon từ user_get qua shop use
-                    if (user_get_coupon.length > 0) {
-                        user_get_coupon.forEach(element => {
-                            if (element._id === req.body.get_couponId) {
-                                user_get_coupon.splice(user_get_coupon.indexOf(element), 1);
-                            }
-                        });
-                    }
+                // if (data !== "") {
+                //     var user_get_coupon = data[0].user_get_coupon;
+                //     var shop_use_coupon = data[0].shop_use_coupon;
+                //     var _today = dateFormat(new Date(), "dd/mm/yyyy");
+                //     //chuyển coupon từ user_get qua shop use
+                //     if (user_get_coupon.length > 0) {
+                //         user_get_coupon.forEach(element => {
+                //             if (element._id === req.body.get_couponId) {
+                //                 user_get_coupon.splice(user_get_coupon.indexOf(element), 1);
+                //             }
+                //         });
+                //     }
 
-                    if (shop_use_coupon.length > 0) {
-                        shop_use_coupon.forEach(element => {
-                            if (element._id.toString() === req.body.couponId) {
+                //     if (shop_use_coupon.length > 0) {
+                //         shop_use_coupon.forEach(element => {
+                //             if (element._id.toString() === req.body.couponId) {
 
-                                _coupon = {
-                                    shop_name: element.coupon.shop_name,
-                                    shop_cover: element.coupon.shop_cover,
-                                    shop_avatar: element.coupon.shop_avatar,
-                                    shop_id: element.coupon.shop_id,
-                                    coupon_info: element.coupon.coupon_info,
-                                    value: element.coupon.value,
-                                    class_user: [
-                                        {
-                                            id: element.coupon.class_user[0].id,
-                                            name: element.coupon.class_user[0].name
-                                        }
-                                    ],
-                                    release_day: element.coupon.release_day,
-                                    time_expire: element.coupon.time_expire,
-                                    the_issuer: [
-                                        {
-                                            id: element.coupon.the_issuer[0].id,
-                                            name: element.coupon.the_issuer[0].name
-                                        }
-                                    ],
-                                    status_coupon: [
-                                        {
-                                            id: 0,
-                                            status: "Đã sử dụng"
-                                        }
-                                    ],
-                                    userid_get_coupon: element.coupon.userid_get_coupon,
-                                    time_user_get: element.coupon.time_user_get,
-                                    time_user_use: _today,
-                                    rating: element.coupon.rating,
-                                    rfeedback: [
-                                        {
-                                            name: element.coupon.rfeedback[0].name,
-                                            id: element.coupon.rfeedback[0].id
-                                        }
-                                    ],
-                                    feedback: element.coupon.feedback,
-                                    approved: element.coupon.approved,
-                                    _id: element.coupon._id
-                                }
+                //                 _coupon = {
+                //                     shop_name: element.coupon.shop_name,
+                //                     shop_cover: element.coupon.shop_cover,
+                //                     shop_avatar: element.coupon.shop_avatar,
+                //                     shop_id: element.coupon.shop_id,
+                //                     coupon_info: element.coupon.coupon_info,
+                //                     value: element.coupon.value,
+                //                     class_user: [
+                //                         {
+                //                             id: element.coupon.class_user[0].id,
+                //                             name: element.coupon.class_user[0].name
+                //                         }
+                //                     ],
+                //                     release_day: element.coupon.release_day,
+                //                     time_expire: element.coupon.time_expire,
+                //                     the_issuer: [
+                //                         {
+                //                             id: element.coupon.the_issuer[0].id,
+                //                             name: element.coupon.the_issuer[0].name
+                //                         }
+                //                     ],
+                //                     status_coupon: [
+                //                         {
+                //                             id: 0,
+                //                             status: "Đã sử dụng"
+                //                         }
+                //                     ],
+                //                     userid_get_coupon: element.coupon.userid_get_coupon,
+                //                     time_user_get: element.coupon.time_user_get,
+                //                     time_user_use: _today,
+                //                     rating: element.coupon.rating,
+                //                     rfeedback: [
+                //                         {
+                //                             name: element.coupon.rfeedback[0].name,
+                //                             id: element.coupon.rfeedback[0].id
+                //                         }
+                //                     ],
+                //                     feedback: element.coupon.feedback,
+                //                     approved: element.coupon.approved,
+                //                     _id: element.coupon._id
+                //                 }
 
-                                the_new = {
-                                    _id: element._id,
-                                    approved: true,
-                                    coupon: _coupon
-                                }
-                                shop_use_coupon.splice(shop_use_coupon.indexOf(element), 1);
-                            }
-                        });
-                    }
+                //                 the_new = {
+                //                     _id: element._id,
+                //                     approved: true,
+                //                     coupon: _coupon
+                //                 }
+                //                 shop_use_coupon.splice(shop_use_coupon.indexOf(element), 1);
+                //             }
+                //         });
+                //     }
 
-                    shop_use_coupon.push(the_new);
-                    data[0].user_get_coupon = user_get_coupon;
-                    data[0].shop_use_coupon = shop_use_coupon;
-                    data[0].save(function (err) {
-                        if (err) {
-                            response = { 'error_code': 2, 'message': err }
-                        } else {
-                            response = { 'error_code': 0, 'message': 'coupon is approved' };
-                        }
-                        res.status(200).json(response);
-                    });
-                }else{
-                    response = { 'error_code': 2, 'message': 'Shop id incorrect' };
+                //     shop_use_coupon.push(the_new);
+                //     data[0].user_get_coupon = user_get_coupon;
+                //     data[0].shop_use_coupon = shop_use_coupon;
+                //     data[0].save(function (err) {
+                //         if (err) {
+                //             response = { 'error_code': 2, 'message': err }
+                //         } else {
+                //             response = { 'error_code': 0, 'message': 'coupon is approved' };
+                //         }
+                //         res.status(200).json(response);
+                //     });
+                // }else{
+                    response = { 'error_code': 2, 'message': data };
                     res.status(200).json(response);
-                }
+                // }
             }
         })
     },
