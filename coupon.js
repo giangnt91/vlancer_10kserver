982b9563c544e291a90cfc3090f24ef570f74fc6
Trường giang <giangkoolpark@gmail.com>
2018-10-13 21:34:17 +0700

    fix api call

--- coupon.js
+++ coupon.js
@@ -109,7 +109,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     //allow connect
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-    var allowedOrigins = ['http://35.240.165.98:8080', 'http://localhost:8080', 'http://localhost:8081', 'http://192.168.1.111:8100', 'https://coupon10k.com', 'http://shop.coupon10k.com'];
+    var allowedOrigins = ['http://35.240.165.98:8080', 'http://localhost:8080', 'http://localhost:8081', 'http://192.168.1.111:8100', 'https://coupon10k.com', 'https://shop.coupon10k.com'];
     var origin = req.headers.origin;
     if (allowedOrigins.indexOf(origin) > -1) {
         res.setHeader('Access-Control-Allow-Origin', origin);
