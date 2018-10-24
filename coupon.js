06b43d46f2589d8a82ad2f3eb230253bcf06f75f
Trường giang <giangkoolpark@gmail.com>
2018-10-24 21:45:55 +0700

    check

--- coupon.js
+++ coupon.js
@@ -84,8 +84,8 @@ io.on('connection', function (socket) {
     })
 
     //mobile
-	socket.on('get_coupon', function(){
-		socket.broadcast.emit('f5_mobile');
+	socket.on('user_get_coupon', function(uid){
+		socket.broadcast.emit('user_mobile', uid);
 	})
 	
     socket.on('user_use_coupon', function (shop_id, user_img, user_name) {
