0111af25b21135185243571fe26a53585a9daad8
Truong giang <giangkoolpark@gmail.com>
2019-04-21 22:02:42 +0700

    update find all hot deal

--- public/controller/hotCtrl.js
+++ public/controller/hotCtrl.js
@@ -28,21 +28,36 @@ module.exports = {
     hotDeal.save(err => {
       if (err) {
         console.log('Tạo mã khuyến mãi có lỗi: ' + err);
-        response = { 'error_code': 1, 'sms': 'error fetching data' };
+        response = {
+          'error_code': 1,
+          'sms': 'error fetching data'
+        };
         res.status(200).json(response);
       } else {
-        response = { 'error_code': 0, 'sms': 'create hot deal success' };
+        response = {
+          'error_code': 0,
+          'sms': 'create hot deal success'
+        };
         res.status(200).json(response);
       }
     })
   },
 
   hotDealGetAll: (req, res) => {
-    hotModel.find((err, data) => {
+    let today = dateFormat(new Date(), "yyyy-mm-dd");
+    let query = {
+      expiredDayIso: {
+        $gte: today
+      }
+    }
+    hotModel.find(query, (err, data) => {
       if (err) {
         console.log('Lỗi lấy danh sách khuyến mãi hot: ' + err);
       } else {
-        response = { 'error_code': 0, 'hots': data };
+        response = {
+          'error_code': 0,
+          'hots': data
+        };
         res.status(200).json(response);
       }
     }).sort({
@@ -54,7 +69,10 @@ module.exports = {
     hotModel.findById(req.body.hotDeal._id, (err, data) => {
       if (err) {
         console.log('Update mã khuyến mãi có lỗi: ' + err);
-        response = { 'error_code': 1, 'sms': 'error fetching data' };
+        response = {
+          'error_code': 1,
+          'sms': 'error fetching data'
+        };
         res.status(200).json(response);
       } else {
         data.urlImage = req.body.hotDeal.urlImage;
@@ -67,7 +85,10 @@ module.exports = {
           if (err) {
             console.log('Lưu khuyến mãi hot: ' + err);
           } else {
-            response = { 'error_code': 0, 'sms': 'update success' };
+            response = {
+              'error_code': 0,
+              'sms': 'update success'
+            };
             res.status(200).json(response);
           }
         })
@@ -76,13 +97,21 @@ module.exports = {
   },
 
   hotDealDelById: (req, res) => {
-    hotModel.findByIdAndRemove({_id: req.body._id}, (err, data) => {
+    hotModel.findByIdAndRemove({
+      _id: req.body._id
+    }, (err, data) => {
       if (err) {
         console.log('Delete mã khuyến mãi có lỗi: ' + err);
-        response = { 'error_code': 1, 'sms': 'error fetching data' };
+        response = {
+          'error_code': 1,
+          'sms': 'error fetching data'
+        };
         res.status(200).json(response);
       } else {
-        response = { 'error_code': 0, 'sms': 'delete success' };
+        response = {
+          'error_code': 0,
+          'sms': 'delete success'
+        };
         res.status(200).json(response);
       }
     })
